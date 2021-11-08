48b4b369b9f5a700b068b295a2107aae9ed37e67
LAPTOP-ONTVNNCH\monti <pavelblogmsg@gmail.com>
2021-11-08 14:13:40 +0200

    ProductListViewModel Additional file

---
+++ Shop/Views/Product/ProductListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Views.Product
+{
+    public class ProductListViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+
+
+
+    }
+}
